5b23773ef0ebe4c8893a995b516a67b2efcd8186
Alex Levin <pinkfloydx20@gmail.com>
2020-05-24 17:35:21 +0300

    Add enviroment variables

--- app/config/settings.py
+++ app/config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'afs2uchyrov1e0t_3-6-mfyrgceo^gikqa5by32*@1uk5goay+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 0))
 
 ALLOWED_HOSTS = ['*']
 
