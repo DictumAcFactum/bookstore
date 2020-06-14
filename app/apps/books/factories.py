77ab952327de0d13737d8433a3aeccd15726ff81
Alex Levin <pinkfloydx20@gmail.com>
2020-06-14 22:02:36 +0300

    fix

--- app/apps/books/factories.py
+++ app/apps/books/factories.py
@@ -4,6 +4,7 @@ from . import models
 
 class BookFactory(factory.Factory):
     author = factory.Faker('name')
+    price = factory.Faker('pydecimal')
 
     class Meta:
         model = models.Book
